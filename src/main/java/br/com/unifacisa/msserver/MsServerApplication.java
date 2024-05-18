e6f96376f658e73c01627f1e29d8a9695190a185
Luís Netto <luisnttt@outlook.com>
2024-05-18 04:17:15 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/unifacisa/msserver/MsServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifacisa.msserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsServerApplication.class, args);
+    }
+
+}
